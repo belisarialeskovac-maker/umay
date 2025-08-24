f1e7d0c207f3eaef25c7babbdbc8998a082fe8eb
Belisaria Leskovac <belisarialeskovac@gmail.com>
2025-08-24 10:08:44 +0000

    Now connect my firebase
    // Import the functions you need from the SDKs

---
+++ src/lib/firebase.ts
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics, isSupported } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAHhSub3vra5b6VFfLBZUfigz8V9NvlpY",
+  authDomain: "project-1a07d.firebaseapp.com",
+  projectId: "project-1a07d",
+  storageBucket: "project-1a07d.firebasestorage.app",
+  messagingSenderId: "69392006548",
+  appId: "1:69392006548:web:3e705fd22098396f90a381",
+  measurementId: "G-ZFPYDM2MCM"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+
+let analytics;
+if (typeof window !== 'undefined') {
+    isSupported().then((supported) => {
+        if (supported) {
+            analytics = getAnalytics(app);
+        }
+    });
+}
+
+export { app, db };
